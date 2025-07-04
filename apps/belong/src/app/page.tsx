3448b8f3c5169bb0c42809b931d50dde51b52aaf
stphils <87964197+stphils@users.noreply.github.com>
2025-07-04 14:41:58 +1000

    Update page.tsx

--- apps/belong/src/app/page.tsx
+++ apps/belong/src/app/page.tsx
@@ -47,7 +47,7 @@ export default async function Home() {
           <script src= "https://unpkg.com/react@16/umd/react.production.min.js"></script>
           <script src= "https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
           <script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
-         
+          <div id="root"></div>
           <Person>
             <PersonImage>
               <SanityImage
