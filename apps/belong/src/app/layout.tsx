9fc99007a145fef94dd1b49f5401ea97100e8f16
stphils <87964197+stphils@users.noreply.github.com>
2025-11-17 14:59:39 +1100

    Update layout.tsx

--- apps/belong/src/app/layout.tsx
+++ apps/belong/src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${linotte.variable} bg-white`}>
       <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Split Screen Layout</title>
