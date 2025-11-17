870fb331693fba48ada7ece906def744e4b587f0
stphils <87964197+stphils@users.noreply.github.com>
2025-11-17 18:01:54 +1100

    Update layout.tsx

--- apps/belong/src/app/layout.tsx
+++ apps/belong/src/app/layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout({
       <head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Split Screen Layout</title>
       </head>
       <body>
