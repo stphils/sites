d1b6cb032ea62215cb67e847369a64f9d4e9c207
stphils <87964197+stphils@users.noreply.github.com>
2025-11-26 10:42:19 +1100

    Update page.tsx

--- apps/belong/src/app/page.tsx
+++ apps/belong/src/app/page.tsx
@@ -6,7 +6,6 @@ import { Logo } from "ui";
 interface Song { title: string; lyrics: string; }
 interface LanguageData { langName: string; translateUrl: string; songs: Song[]; }
 interface AllLyrics { [key: string]: LanguageData; }
-const CONTACT_INTERNAL_HTML = `<h2>Contact Us</h2><p>Thank you for your interest! We have opened our contact form in a new tab for you.</p><p>If the form did not open automatically, please <a href="${CONTACT_IFRAME_SRC}" target="_blank" style="text-decoration: underline; color: blue;">click here</a>.</p><p>We look forward to hearing from you.</p>`;
 const allLyrics: AllLyrics = {
   'en': {
     langName: 'English',
@@ -285,6 +284,7 @@ our Lord Emmanuel`
 
 const CONTACT_TITLE = 'Contact St Phils';
 const CONTACT_IFRAME_SRC = 'https://stphilseastwood.elvanto.com.au/form/2840a4a4-1e93-454e-890b-5e358d69b811';
+const CONTACT_INTERNAL_HTML = `<h2>Contact Us</h2><p>Thank you for your interest! We have opened our contact form in a new tab for you.</p><p>If the form did not open automatically, please <a href="${CONTACT_IFRAME_SRC}" target="_blank" style="text-decoration: underline; color: blue;">click here</a>.</p><p>We look forward to hearing from you.</p>`;
 
 
 export default function Home() {
